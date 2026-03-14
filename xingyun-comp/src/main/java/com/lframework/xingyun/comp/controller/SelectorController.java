09c69ad8cfeafeaf73e204d6755ad3b3947fe073
bogewang <foren236598164@aliyun.com>
2026-03-14 17:39:19 +0800

    /list/tenant

--- xingyun-comp/src/main/java/com/lframework/xingyun/comp/controller/SelectorController.java
+++ xingyun-comp/src/main/java/com/lframework/xingyun/comp/controller/SelectorController.java
@@ -1,21 +1,28 @@
 package com.lframework.xingyun.comp.controller;
 
 import com.lframework.starter.common.utils.CollectionUtil;
-import com.lframework.starter.web.core.controller.DefaultBaseController;
 import com.lframework.starter.web.core.components.resp.InvokeResult;
 import com.lframework.starter.web.core.components.resp.InvokeResultBuilder;
-import com.lframework.xingyun.comp.bo.dic.CitySelectorBo;
+import com.lframework.starter.web.core.components.resp.PageResult;
+import com.lframework.starter.web.core.controller.DefaultBaseController;
+import com.lframework.starter.web.inner.bo.system.tenant.QueryTenantBo;
 import com.lframework.starter.web.inner.dto.dic.city.DicCityDto;
+import com.lframework.starter.web.inner.entity.Tenant;
 import com.lframework.starter.web.inner.service.DicCityService;
+import com.lframework.starter.web.inner.service.TenantService;
+import com.lframework.starter.web.inner.vo.system.tenant.QueryTenantVo;
+import com.lframework.xingyun.comp.bo.dic.CitySelectorBo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 数据选择器
@@ -28,22 +35,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/selector")
 public class SelectorController extends DefaultBaseController {
 
-  @Autowired
-  private DicCityService dicCityService;
+    @Autowired
+    private DicCityService dicCityService;
 
-  /**
-   * 城市数据
-   */
-  @ApiOperation("城市数据")
-  @GetMapping("/city")
-  public InvokeResult<List<CitySelectorBo>> dicCity() {
+    @Autowired
+    private TenantService tenantService;
 
-    List<DicCityDto> datas = dicCityService.getAll();
-    List<CitySelectorBo> results = CollectionUtil.emptyList();
-    if (!CollectionUtil.isEmpty(datas)) {
-      results = datas.stream().map(CitySelectorBo::new).collect(Collectors.toList());
+    /**
+     * 城市数据
+     */
+    @ApiOperation("城市数据")
+    @GetMapping("/city")
+    public InvokeResult<List<CitySelectorBo>> dicCity() {
+
+        List<DicCityDto> datas = dicCityService.getAll();
+        List<CitySelectorBo> results = CollectionUtil.emptyList();
+        if (!CollectionUtil.isEmpty(datas)) {
+            results = datas.stream().map(CitySelectorBo::new).collect(Collectors.toList());
+        }
+
+        return InvokeResultBuilder.success(results);
     }
 
-    return InvokeResultBuilder.success(results);
-  }
+    /**
+     * 租户
+     */
+    @ApiOperation("租户")
+    @GetMapping("/list/tenant")
+    public InvokeResult<List<QueryTenantBo>> tenant(@Valid QueryTenantVo vo) {
+
+        PageResult<Tenant> pageResult = tenantService.query(getPageIndex(vo), getPageSize(vo), vo);
+
+        List<QueryTenantBo> datas = pageResult.getDatas().stream()
+                .map(item -> {
+                    Tenant tenant = new Tenant();
+                    tenant.setName(item.getName());
+                    return tenant;
+                })
+                .map(QueryTenantBo::new)
+                .collect(Collectors.toList());
+
+
+        return InvokeResultBuilder.success(datas);
+    }
 }
