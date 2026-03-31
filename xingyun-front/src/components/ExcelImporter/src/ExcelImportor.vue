71cacebb84441e49b2ff12d2d5f52c93c1b7e5e7
wangbo <236598164@qq.com>
2026-03-31 21:15:13 +0800

    完成后是否关闭对话框

--- xingyun-front/src/components/ExcelImporter/src/ExcelImportor.vue
+++ xingyun-front/src/components/ExcelImporter/src/ExcelImportor.vue
@@ -64,12 +64,12 @@
   </div>
 </template>
 <script>
-  import { defineComponent } from 'vue';
-  import { InboxOutlined } from '@ant-design/icons-vue';
-  import * as api from '@/api/components';
-  import { isEmpty, uuid } from '@/utils/utils';
+import {defineComponent} from 'vue';
+import {InboxOutlined} from '@ant-design/icons-vue';
+import * as api from '@/api/components';
+import {isEmpty, uuid} from '@/utils/utils';
 
-  export default defineComponent({
+export default defineComponent({
     name: 'ExcelImporter',
     components: {
       InboxOutlined,
@@ -103,7 +103,7 @@
       // 完成后是否关闭对话框
       closeAfterFinish: {
         type: Boolean,
-        default: false,
+        default: true,
       },
     },
     setup() {
