7e4733ef8c4ccda37721805f94d5946af8bcee1e
wangbo <236598164@qq.com>
2026-05-04 17:54:31 +0800

    ADD COLUMN `biz_type`

--- xingyun-api/src/main/resources/db/migration/tenant/V2.0.sql
+++ xingyun-api/src/main/resources/db/migration/tenant/V2.0.sql
@@ -8,7 +8,7 @@ update sys_menu set sys_module_id = 3 where code = '2000011';
 ALTER TABLE `tbl_print_template` ADD COLUMN `lang` VARCHAR(20) NULL DEFAULT NULL COMMENT '语言';
 
 -- 业务类型
-ALTER TABLE `tbl_print_template` ADD COLUMN `bizType` VARCHAR(20) NULL DEFAULT NULL COMMENT '业务类型';
+ALTER TABLE `tbl_print_template` ADD COLUMN `biz_type` VARCHAR(20) NULL DEFAULT NULL COMMENT '业务类型';
 
 -- 版本
 ALTER TABLE `tbl_print_template` ADD COLUMN `version` VARCHAR(20) NULL DEFAULT NULL COMMENT '版本';
