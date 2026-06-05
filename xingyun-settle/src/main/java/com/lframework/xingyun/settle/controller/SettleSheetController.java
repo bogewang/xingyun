bfe2a5ccd16d9969c80e0c49587ec1b226b740e7
wangbo <236598164@qq.com>
2026-06-05 13:55:56 +0800

    格式化

--- xingyun-settle/src/main/java/com/lframework/xingyun/settle/controller/SettleSheetController.java
+++ xingyun-settle/src/main/java/com/lframework/xingyun/settle/controller/SettleSheetController.java
@@ -46,180 +46,180 @@ import java.util.stream.Collectors;
 @Slf4j
 public class SettleSheetController extends DefaultBaseController {
 
-  @Autowired
-  private SettleSheetService settleSheetService;
+    @Autowired
+    private SettleSheetService settleSheetService;
 
-  @Autowired
-  private SettleSheetSummaryService settleSheetSummaryService;
+    @Autowired
+    private SettleSheetSummaryService settleSheetSummaryService;
 
-  @Autowired
-  private ReceiveSheetService receiveSheetService;
+    @Autowired
+    private ReceiveSheetService receiveSheetService;
 
-  /**
-   * 供应商结算单列表
-   */
-  @ApiOperation("供应商结算单列表")
-  @HasPermission({"settle:sheet:query"})
-  @GetMapping("/query")
-  public InvokeResult<PageResult<QuerySettleSheetBo>> query(@Valid QuerySettleSheetVo vo) {
+    /**
+     * 供应商结算单列表
+     */
+    @ApiOperation("供应商结算单列表")
+    @HasPermission({"settle:sheet:query"})
+    @GetMapping("/query")
+    public InvokeResult<PageResult<QuerySettleSheetBo>> query(@Valid QuerySettleSheetVo vo) {
 
-    PageResult<SettleSheet> pageResult = settleSheetService.query(getPageIndex(vo), getPageSize(vo),
-        vo);
+        PageResult<SettleSheet> pageResult = settleSheetService.query(getPageIndex(vo), getPageSize(vo),
+                vo);
 
-    List<SettleSheet> datas = pageResult.getDatas();
-    List<QuerySettleSheetBo> results = null;
+        List<SettleSheet> datas = pageResult.getDatas();
+        List<QuerySettleSheetBo> results = null;
 
-    if (!CollectionUtil.isEmpty(datas)) {
-      results = datas.stream().map(QuerySettleSheetBo::new).collect(Collectors.toList());
+        if (!CollectionUtil.isEmpty(datas)) {
+            results = datas.stream().map(QuerySettleSheetBo::new).collect(Collectors.toList());
+        }
+
+        return InvokeResultBuilder.success(PageResultUtil.rebuild(pageResult, results));
     }
 
-    return InvokeResultBuilder.success(PageResultUtil.rebuild(pageResult, results));
-  }
+    /**
+     * 供应商结算汇总
+     */
+    @ApiOperation("供应商结算汇总")
+    @HasPermission({"settle:sheet:query"})
+    @GetMapping("/summary")
+    public InvokeResult<List<SettleSheetSummaryBo>> summary(@Valid QuerySettleSheetSummaryVo vo) {
 
-  /**
-   * 供应商结算汇总
-   */
-  @ApiOperation("供应商结算汇总")
-  @HasPermission({"settle:sheet:query"})
-  @GetMapping("/summary")
-  public InvokeResult<List<SettleSheetSummaryBo>> summary(@Valid QuerySettleSheetSummaryVo vo) {
-
-      try {
-          return InvokeResultBuilder.success(settleSheetSummaryService.query(vo));
-      } catch (Exception e) {
-          log.error("请求出错", e);
-          return InvokeResultBuilder.fail(e.getMessage(), null);
-      }
-  }
-
-  @ApiOperation("查询收货单对账结算扩展信息")
-  @HasPermission({"settle:sheet:query"})
-  @PostMapping("/receive-sheet-settle-infos")
-  public InvokeResult<List<ReceiveSheetSettleInfoBo>> queryReceiveSheetSettleInfos(@RequestBody @Valid QueryReceiveSheetVo vo) {
-    try {
-      PageResult<ReceiveSheet> pageResult = receiveSheetService.query(getPageIndex(vo), getPageSize(vo), vo);
-
-      List<ReceiveSheetSettleInfoBo> data = settleSheetService.queryReceiveSheetSettleInfos(pageResult.getDatas());
-      return InvokeResultBuilder.success(data);
-    } catch (Exception e) {
-      log.error("请求出错", e);
-      return InvokeResultBuilder.fail(e.getMessage(), null);
+        try {
+            return InvokeResultBuilder.success(settleSheetSummaryService.query(vo));
+        } catch (Exception e) {
+            log.error("请求出错", e);
+            return InvokeResultBuilder.fail(e.getMessage(), null);
+        }
     }
-  }
 
-  /**
-   * 导出
-   */
-  @ApiOperation("导出")
-  @HasPermission({"settle:sheet:export"})
-  @PostMapping("/export")
-  public InvokeResult<Void> export(@Valid QuerySettleSheetVo vo) {
+    @ApiOperation("查询收货单对账结算扩展信息")
+    @HasPermission({"settle:sheet:query"})
+    @PostMapping("/receive-sheet-settle-infos")
+    public InvokeResult<List<ReceiveSheetSettleInfoBo>> queryReceiveSheetSettleInfos(@RequestBody @Valid QueryReceiveSheetVo vo) {
+        try {
+            PageResult<ReceiveSheet> pageResult = receiveSheetService.query(getPageIndex(vo), getPageSize(vo), vo);
 
-    ExportTaskUtil.exportTask("供应商结算单信息", SettleSheetExportTaskWorker.class, vo);
+            List<ReceiveSheetSettleInfoBo> data = settleSheetService.queryReceiveSheetSettleInfos(pageResult.getDatas());
+            return InvokeResultBuilder.success(data);
+        } catch (Exception e) {
+            log.error("请求出错", e);
+            return InvokeResultBuilder.fail(e.getMessage(), null);
+        }
+    }
 
-    return InvokeResultBuilder.success();
-  }
+    /**
+     * 导出
+     */
+    @ApiOperation("导出")
+    @HasPermission({"settle:sheet:export"})
+    @PostMapping("/export")
+    public InvokeResult<Void> export(@Valid QuerySettleSheetVo vo) {
 
-  /**
-   * 根据ID查询
-   */
-  @ApiOperation("根据ID查询")
-  @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
-  @HasPermission({"settle:sheet:query"})
-  @GetMapping
-  public InvokeResult<GetSettleSheetBo> findById(
-      @NotBlank(message = "供应商结算单ID不能为空！") String id) {
+        ExportTaskUtil.exportTask("供应商结算单信息", SettleSheetExportTaskWorker.class, vo);
 
-    SettleSheetFullDto data = settleSheetService.getDetail(id);
+        return InvokeResultBuilder.success();
+    }
 
-    GetSettleSheetBo result = new GetSettleSheetBo(data);
+    /**
+     * 根据ID查询
+     */
+    @ApiOperation("根据ID查询")
+    @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
+    @HasPermission({"settle:sheet:query"})
+    @GetMapping
+    public InvokeResult<GetSettleSheetBo> findById(
+            @NotBlank(message = "供应商结算单ID不能为空！") String id) {
 
-    return InvokeResultBuilder.success(result);
-  }
+        SettleSheetFullDto data = settleSheetService.getDetail(id);
 
-  /**
-   * 创建供应商结算单
-   */
-  @ApiOperation("创建供应商结算单")
-  @HasPermission({"settle:sheet:add"})
-  @PostMapping
-  public InvokeResult<String> create(@RequestBody @Valid CreateSettleSheetVo vo) {
+        GetSettleSheetBo result = new GetSettleSheetBo(data);
 
-    vo.validate();
+        return InvokeResultBuilder.success(result);
+    }
 
-    String id = settleSheetService.create(vo);
+    /**
+     * 创建供应商结算单
+     */
+    @ApiOperation("创建供应商结算单")
+    @HasPermission({"settle:sheet:add"})
+    @PostMapping
+    public InvokeResult<String> create(@RequestBody @Valid CreateSettleSheetVo vo) {
 
-    return InvokeResultBuilder.success(id);
-  }
+        vo.validate();
 
-  /**
-   * 修改供应商结算单
-   */
-  @ApiOperation("修改供应商结算单")
-  @HasPermission({"settle:sheet:modify"})
-  @PutMapping
-  public InvokeResult<Void> update(@RequestBody @Valid UpdateSettleSheetVo vo) {
+        String id = settleSheetService.create(vo);
 
-    vo.validate();
+        return InvokeResultBuilder.success(id);
+    }
 
-    settleSheetService.update(vo);
+    /**
+     * 修改供应商结算单
+     */
+    @ApiOperation("修改供应商结算单")
+    @HasPermission({"settle:sheet:modify"})
+    @PutMapping
+    public InvokeResult<Void> update(@RequestBody @Valid UpdateSettleSheetVo vo) {
 
-    return InvokeResultBuilder.success();
-  }
+        vo.validate();
 
-  /**
-   * 审核通过供应商结算单
-   */
-  @ApiOperation("审核通过供应商结算单")
-  @HasPermission({"settle:sheet:approve"})
-  @PatchMapping("/approve/pass")
-  public InvokeResult<Void> approvePass(@RequestBody @Valid ApprovePassSettleSheetVo vo) {
+        settleSheetService.update(vo);
 
-    settleSheetService.approvePass(vo);
+        return InvokeResultBuilder.success();
+    }
 
-    return InvokeResultBuilder.success();
-  }
+    /**
+     * 审核通过供应商结算单
+     */
+    @ApiOperation("审核通过供应商结算单")
+    @HasPermission({"settle:sheet:approve"})
+    @PatchMapping("/approve/pass")
+    public InvokeResult<Void> approvePass(@RequestBody @Valid ApprovePassSettleSheetVo vo) {
 
-  /**
-   * 直接审核通过供应商结算单
-   */
-  @ApiOperation("直接审核通过供应商结算单")
-  @HasPermission({"settle:sheet:approve"})
-  @PostMapping("/approve/pass/direct")
-  public InvokeResult<Void> directApprovePass(@RequestBody @Valid CreateSettleSheetVo vo) {
+        settleSheetService.approvePass(vo);
 
-    vo.validate();
+        return InvokeResultBuilder.success();
+    }
 
-    settleSheetService.directApprovePass(vo);
+    /**
+     * 直接审核通过供应商结算单
+     */
+    @ApiOperation("直接审核通过供应商结算单")
+    @HasPermission({"settle:sheet:approve"})
+    @PostMapping("/approve/pass/direct")
+    public InvokeResult<Void> directApprovePass(@RequestBody @Valid CreateSettleSheetVo vo) {
 
-    return InvokeResultBuilder.success();
-  }
+        vo.validate();
 
-  /**
-   * 审核拒绝供应商结算单
-   */
-  @ApiOperation("审核拒绝供应商结算单")
-  @HasPermission({"settle:sheet:approve"})
-  @PatchMapping("/approve/refuse")
-  public InvokeResult<Void> approveRefuse(@RequestBody @Valid ApproveRefuseSettleSheetVo vo) {
+        settleSheetService.directApprovePass(vo);
 
-    settleSheetService.approveRefuse(vo);
+        return InvokeResultBuilder.success();
+    }
 
-    return InvokeResultBuilder.success();
-  }
+    /**
+     * 审核拒绝供应商结算单
+     */
+    @ApiOperation("审核拒绝供应商结算单")
+    @HasPermission({"settle:sheet:approve"})
+    @PatchMapping("/approve/refuse")
+    public InvokeResult<Void> approveRefuse(@RequestBody @Valid ApproveRefuseSettleSheetVo vo) {
 
-  /**
-   * 删除供应商结算单
-   */
-  @ApiOperation("删除供应商结算单")
-  @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
-  @HasPermission({"settle:sheet:delete"})
-  @DeleteMapping
-  public InvokeResult<Void> deleteById(@NotBlank(message = "供应商结算单ID不能为空！") String id) {
+        settleSheetService.approveRefuse(vo);
 
-    settleSheetService.deleteById(id);
+        return InvokeResultBuilder.success();
+    }
 
-    return InvokeResultBuilder.success();
-  }
+    /**
+     * 删除供应商结算单
+     */
+    @ApiOperation("删除供应商结算单")
+    @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
+    @HasPermission({"settle:sheet:delete"})
+    @DeleteMapping
+    public InvokeResult<Void> deleteById(@NotBlank(message = "供应商结算单ID不能为空！") String id) {
+
+        settleSheetService.deleteById(id);
+
+        return InvokeResultBuilder.success();
+    }
 
 }
