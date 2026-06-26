050d59e177e20e88bed7066dbaeb025857eb07ed
wangbo <236598164@qq.com>
2026-06-26 10:20:18 +0800

    格式化

--- xingyun-sc/src/main/java/com/lframework/xingyun/sc/service/stock/ProductStockService.java
+++ xingyun-sc/src/main/java/com/lframework/xingyun/sc/service/stock/ProductStockService.java
@@ -11,54 +11,54 @@ import java.util.List;
 
 public interface ProductStockService extends BaseMpService<ProductStock> {
 
-  /**
-   * 查询列表
-   *
-   * @param pageIndex
-   * @param pageSize
-   * @param vo
-   * @return
-   */
-  PageResult<ProductStock> query(Integer pageIndex, Integer pageSize, QueryProductStockVo vo);
+    /**
+     * 查询列表
+     *
+     * @param pageIndex
+     * @param pageSize
+     * @param vo
+     * @return
+     */
+    PageResult<ProductStock> query(Integer pageIndex, Integer pageSize, QueryProductStockVo vo);
 
-  /**
-   * 查询列表
-   *
-   * @param vo
-   * @return
-   */
-  List<ProductStock> query(QueryProductStockVo vo);
+    /**
+     * 查询列表
+     *
+     * @param vo
+     * @return
+     */
+    List<ProductStock> query(QueryProductStockVo vo);
 
-  /**
-   * 根据商品ID、仓库ID查询
-   *
-   * @param productId
-   * @param scId
-   * @return
-   */
-  ProductStock getByProductIdAndScId(String productId, String scId);
+    /**
+     * 根据商品ID、仓库ID查询
+     *
+     * @param productId
+     * @param scId
+     * @return
+     */
+    ProductStock getByProductIdAndScId(String productId, String scId);
 
-  /**
-   * 根据商品ID、仓库ID查询
-   *
-   * @param productIds
-   * @param scId
-   * @return
-   */
-  List<ProductStock> getByProductIdsAndScId(List<String> productIds, String scId,
-      Integer productType);
+    /**
+     * 根据商品ID、仓库ID查询
+     *
+     * @param productIds
+     * @param scId
+     * @return
+     */
+    List<ProductStock> getByProductIdsAndScId(List<String> productIds, String scId,
+                                              Integer productType);
 
-  /**
-   * 入库
-   *
-   * @param vo
-   */
-  ProductStockChangeDto addStock(AddProductStockVo vo);
+    /**
+     * 入库
+     *
+     * @param vo
+     */
+    ProductStockChangeDto addStock(AddProductStockVo vo);
 
-  /**
-   * 出库
-   *
-   * @param vo
-   */
-  ProductStockChangeDto subStock(SubProductStockVo vo);
+    /**
+     * 出库
+     *
+     * @param vo
+     */
+    ProductStockChangeDto subStock(SubProductStockVo vo);
 }
