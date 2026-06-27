1932aa8aaae3d557ba9ec60b4a5393a67c65eb0d
wangbo <236598164@qq.com>
2026-06-27 09:26:05 +0800

    格式化

--- xingyun-sc/src/main/java/com/lframework/xingyun/sc/mappers/ProductStockMapper.java
+++ xingyun-sc/src/main/java/com/lframework/xingyun/sc/mappers/ProductStockMapper.java
@@ -8,8 +8,10 @@ import com.lframework.starter.web.core.annotations.sort.Sorts;
 import com.lframework.starter.web.inner.components.permission.ProductDataPermissionDataPermissionType;
 import com.lframework.xingyun.sc.entity.ProductStock;
 import com.lframework.xingyun.sc.vo.stock.QueryProductStockVo;
+
 import java.math.BigDecimal;
 import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 /**
@@ -22,75 +24,75 @@ import org.apache.ibatis.annotations.Param;
  */
 public interface ProductStockMapper extends BaseMapper<ProductStock> {
 
-  /**
-   * 查询列表
-   *
-   * @param vo
-   * @return
-   */
-  @Sorts({
-      @Sort(value = "scCode", alias = "sc.code"),
-      @Sort(value = "productCode", alias = "g.code"),
-      @Sort(value = "stockNum", alias = "gs.stock_num"),
-  })
-  @DataPermissions(type = ProductDataPermissionDataPermissionType.class, value = {
-      @DataPermission(template = "product", alias = "g"),
-      @DataPermission(template = "brand", alias = "b"),
-      @DataPermission(template = "category", alias = "c")
-  })
-  List<ProductStock> query(@Param("vo") QueryProductStockVo vo);
+    /**
+     * 查询列表
+     *
+     * @param vo
+     * @return
+     */
+    @Sorts({
+            @Sort(value = "scCode", alias = "sc.code"),
+            @Sort(value = "productCode", alias = "g.code"),
+            @Sort(value = "stockNum", alias = "gs.stock_num"),
+    })
+    @DataPermissions(type = ProductDataPermissionDataPermissionType.class, value = {
+            @DataPermission(template = "product", alias = "g"),
+            @DataPermission(template = "brand", alias = "b"),
+            @DataPermission(template = "category", alias = "c")
+    })
+    List<ProductStock> query(@Param("vo") QueryProductStockVo vo);
 
-  /**
-   * 根据商品ID、仓库ID查询
-   *
-   * @param productId
-   * @param scId
-   * @return
-   */
-  ProductStock getByProductIdAndScId(@Param("productId") String productId,
-      @Param("scId") String scId);
+    /**
+     * 根据商品ID、仓库ID查询
+     *
+     * @param productId
+     * @param scId
+     * @return
+     */
+    ProductStock getByProductIdAndScId(@Param("productId") String productId,
+                                       @Param("scId") String scId);
 
-  /**
-   * 根据商品ID、仓库ID查询
-   *
-   * @param productIds
-   * @param scId
-   * @return
-   */
-  List<ProductStock> getByProductIdsAndScId(@Param("productIds") List<String> productIds,
-      @Param("scId") String scId, @Param("productType") Integer productType);
+    /**
+     * 根据商品ID、仓库ID查询
+     *
+     * @param productIds
+     * @param scId
+     * @return
+     */
+    List<ProductStock> getByProductIdsAndScId(@Param("productIds") List<String> productIds,
+                                              @Param("scId") String scId, @Param("productType") Integer productType);
 
-  /**
-   * 入库
-   *
-   * @param productId
-   * @param scId
-   * @param stockNum
-   * @param taxAmount
-   * @param oriStockNum
-   * @param oriTaxAmount
-   * @return
-   */
-  int addStock(@Param("productId") String productId, @Param("scId") String scId,
-      @Param("stockNum") BigDecimal stockNum,
-      @Param("taxAmount") BigDecimal taxAmount,
-      @Param("oriStockNum") BigDecimal oriStockNum, @Param("oriTaxAmount") BigDecimal oriTaxAmount,
-      @Param("reCalcCostPrice") boolean reCalcCostPrice);
+    /**
+     * 入库
+     *
+     * @param productId
+     * @param scId
+     * @param stockNum
+     * @param taxAmount
+     * @param oriStockNum
+     * @param oriTaxAmount
+     * @return
+     */
+    int addStock(@Param("productId") String productId, @Param("scId") String scId,
+                 @Param("stockNum") BigDecimal stockNum,
+                 @Param("taxAmount") BigDecimal taxAmount,
+                 @Param("oriStockNum") BigDecimal oriStockNum, @Param("oriTaxAmount") BigDecimal oriTaxAmount,
+                 @Param("reCalcCostPrice") boolean reCalcCostPrice);
 
-  /**
-   * 出库
-   *
-   * @param productId
-   * @param scId
-   * @param stockNum
-   * @param taxAmount
-   * @param oriStockNum
-   * @param oriTaxAmount
-   * @return
-   */
-  int subStock(@Param("productId") String productId, @Param("scId") String scId,
-      @Param("stockNum") BigDecimal stockNum,
-      @Param("taxAmount") BigDecimal taxAmount,
-      @Param("oriStockNum") BigDecimal oriStockNum, @Param("oriTaxAmount") BigDecimal oriTaxAmount,
-      @Param("reCalcCostPrice") boolean reCalcCostPrice);
+    /**
+     * 出库
+     *
+     * @param productId
+     * @param scId
+     * @param stockNum
+     * @param taxAmount
+     * @param oriStockNum
+     * @param oriTaxAmount
+     * @return
+     */
+    int subStock(@Param("productId") String productId, @Param("scId") String scId,
+                 @Param("stockNum") BigDecimal stockNum,
+                 @Param("taxAmount") BigDecimal taxAmount,
+                 @Param("oriStockNum") BigDecimal oriStockNum, @Param("oriTaxAmount") BigDecimal oriTaxAmount,
+                 @Param("reCalcCostPrice") boolean reCalcCostPrice);
 }
