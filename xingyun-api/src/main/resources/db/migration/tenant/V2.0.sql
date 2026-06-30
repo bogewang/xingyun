8aaa6a74c0202f82f2ba7e2d9d4ee039b4562bba
bogewang <236598164@qq.com>
2026-06-30 08:32:06 +0800

    delete from base_data_store_center where id = '2070120206814023680';

--- xingyun-api/src/main/resources/db/migration/tenant/V2.0.sql
+++ xingyun-api/src/main/resources/db/migration/tenant/V2.0.sql
@@ -139,6 +139,7 @@ ALTER TABLE settle_check_sheet modify end_date date COMMENT '截止日期';
 
 
 -- 0625
+delete from base_data_store_center where id = '2070120206814023680';
 INSERT INTO `base_data_store_center` (`id`, `code`, `name`, `contact`, `telephone`, `city_id`, `address`, `people_num`, `available`, `description`, `create_by`, `create_by_id`, `create_time`, `update_by`, `update_by_id`, `update_time`) VALUES ('2070120206814023680', 'CK26062500001', '默认仓库', NULL, NULL, NULL, NULL, NULL, 1, '', '系统管理员', '1', '2026-06-25 20:21:33', '系统管理员', '1', '2026-06-25 20:21:33');
 -- 仓库必填
 UPDATE tbl_sale_out_sheet SET sc_id = '2070120206814023680';
