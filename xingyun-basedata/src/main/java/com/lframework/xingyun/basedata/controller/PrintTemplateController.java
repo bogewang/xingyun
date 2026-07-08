f7d1778579645bc768aa7f24edf504ef04a7fd12
bogewang <236598164@qq.com>
2026-07-09 07:28:14 +0800

    格式化

--- xingyun-basedata/src/main/java/com/lframework/xingyun/basedata/controller/PrintTemplateController.java
+++ xingyun-basedata/src/main/java/com/lframework/xingyun/basedata/controller/PrintTemplateController.java
@@ -26,11 +26,13 @@ import com.lframework.xingyun.basedata.vo.print.UpdatePrintTemplateVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -51,167 +53,167 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/basedata/print/template")
 public class PrintTemplateController extends DefaultBaseController {
 
-  @Autowired
-  private PrintTemplateService printTemplateService;
+    @Autowired
+    private PrintTemplateService printTemplateService;
 
-  @Autowired
-  private PrintTemplateCompService printTemplateCompService;
+    @Autowired
+    private PrintTemplateCompService printTemplateCompService;
 
-  /**
-   * 查询列表
-   */
-  @ApiOperation("查询列表")
-  @HasPermission({"base-data:print-template:query"})
-  @GetMapping("/query")
-  public InvokeResult<PageResult<QueryPrintTemplateBo>> query(@Valid QueryPrintTemplateVo vo) {
+    /**
+     * 查询列表
+     */
+    @ApiOperation("查询列表")
+    @HasPermission({"base-data:print-template:query"})
+    @GetMapping("/query")
+    public InvokeResult<PageResult<QueryPrintTemplateBo>> query(@Valid QueryPrintTemplateVo vo) {
 
-    PageResult<PrintTemplate> pageResult = printTemplateService.query(getPageIndex(vo),
-        getPageSize(vo), vo);
+        PageResult<PrintTemplate> pageResult = printTemplateService.query(getPageIndex(vo),
+                getPageSize(vo), vo);
 
-    List<PrintTemplate> datas = pageResult.getDatas();
-    List<QueryPrintTemplateBo> results = null;
+        List<PrintTemplate> datas = pageResult.getDatas();
+        List<QueryPrintTemplateBo> results = null;
 
-    if (!CollectionUtil.isEmpty(datas)) {
-      results = datas.stream().map(QueryPrintTemplateBo::new).collect(Collectors.toList());
+        if (!CollectionUtil.isEmpty(datas)) {
+            results = datas.stream().map(QueryPrintTemplateBo::new).collect(Collectors.toList());
+        }
+
+        return InvokeResultBuilder.success(PageResultUtil.rebuild(pageResult, results));
     }
 
-    return InvokeResultBuilder.success(PageResultUtil.rebuild(pageResult, results));
-  }
+    /**
+     * 根据ID查询
+     */
+    @ApiOperation("根据ID查询")
+    @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
+    @HasPermission({"base-data:print-template:query"})
+    @GetMapping
+    public InvokeResult<GetPrintTemplateBo> get(@NotNull(message = "ID不能为空！") Integer id) {
 
-  /**
-   * 根据ID查询
-   */
-  @ApiOperation("根据ID查询")
-  @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
-  @HasPermission({"base-data:print-template:query"})
-  @GetMapping
-  public InvokeResult<GetPrintTemplateBo> get(@NotNull(message = "ID不能为空！") Integer id) {
+        PrintTemplate data = printTemplateService.findById(id);
+        if (data == null) {
+            throw new DefaultClientException("打印模板不存在！");
+        }
 
-    PrintTemplate data = printTemplateService.findById(id);
-    if (data == null) {
-      throw new DefaultClientException("打印模板不存在！");
+        GetPrintTemplateBo result = new GetPrintTemplateBo(data);
+
+        return InvokeResultBuilder.success(result);
     }
 
-    GetPrintTemplateBo result = new GetPrintTemplateBo(data);
+    /**
+     * 删除
+     */
+    @ApiOperation("删除")
+    @HasPermission({"base-data:print-template:delete"})
+    @DeleteMapping
+    public InvokeResult<Void> deleteById(@NotNull(message = "ID不能为空！") Integer id) {
 
-    return InvokeResultBuilder.success(result);
-  }
+        printTemplateService.deleteById(id);
 
-  /**
-   * 删除
-   */
-  @ApiOperation("删除")
-  @HasPermission({"base-data:print-template:delete"})
-  @DeleteMapping
-  public InvokeResult<Void> deleteById(@NotNull(message = "ID不能为空！") Integer id) {
+        printTemplateService.cleanCacheByKey(id);
 
-    printTemplateService.deleteById(id);
-
-    printTemplateService.cleanCacheByKey(id);
-
-    return InvokeResultBuilder.success();
-  }
-
-  /**
-   * 新增
-   */
-  @ApiOperation("新增")
-  @HasPermission({"base-data:print-template:add"})
-  @PostMapping
-  public InvokeResult<Void> create(@Valid CreatePrintTemplateVo vo) {
-
-    printTemplateService.create(vo);
-
-    return InvokeResultBuilder.success();
-  }
-
-  /**
-   * 复制
-   */
-  @ApiOperation("复制")
-  @HasPermission({"base-data:print-template:add"})
-  @PostMapping("/copy")
-  public InvokeResult<Integer> copy(@Valid CopyPrintTemplateVo vo) {
-
-    Integer id = printTemplateService.copy(vo);
-
-    return InvokeResultBuilder.success(id);
-  }
-
-  /**
-   * 修改
-   */
-  @ApiOperation("修改")
-  @HasPermission({"base-data:print-template:modify"})
-  @PutMapping
-  public InvokeResult<Void> update(@Valid UpdatePrintTemplateVo vo) {
-
-    printTemplateService.update(vo);
-
-    return InvokeResultBuilder.success();
-  }
-
-  /**
-   * 查询设置
-   */
-  @ApiOperation("查询设置")
-  @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
-  @GetMapping("/setting")
-  public InvokeResult<GetPrintTemplateSettingBo> getSetting(
-      @NotNull(message = "ID不能为空！") Integer id) {
-
-    PrintTemplate data = printTemplateService.findById(id);
-    if (data == null) {
-      throw new DefaultClientException("打印模板不存在！");
+        return InvokeResultBuilder.success();
     }
 
-    GetPrintTemplateSettingBo result = new GetPrintTemplateSettingBo(data);
+    /**
+     * 新增
+     */
+    @ApiOperation("新增")
+    @HasPermission({"base-data:print-template:add"})
+    @PostMapping
+    public InvokeResult<Void> create(@Valid CreatePrintTemplateVo vo) {
 
-    return InvokeResultBuilder.success(result);
-  }
+        printTemplateService.create(vo);
 
-  /**
-   * 保存设置
-   */
-  @ApiOperation("保存设置")
-  @HasPermission({"base-data:print-template:query"})
-  @PutMapping("/setting")
-  public InvokeResult<Void> updateSetting(@Valid UpdatePrintTemplateSettingVo vo) {
+        return InvokeResultBuilder.success();
+    }
 
-    printTemplateService.updateSetting(vo);
+    /**
+     * 复制
+     */
+    @ApiOperation("复制")
+    @HasPermission({"base-data:print-template:add"})
+    @PostMapping("/copy")
+    public InvokeResult<Integer> copy(@Valid CopyPrintTemplateVo vo) {
 
-    return InvokeResultBuilder.success();
-  }
+        Integer id = printTemplateService.copy(vo);
 
-  @ApiOperation("设置示例数据")
-  @HasPermission({"base-data:print-template:modify"})
-  @PutMapping("/setting/demo")
-  public InvokeResult<Void> updateDemoData(@Valid UpdatePrintTemplateDemoDataVo vo) {
+        return InvokeResultBuilder.success(id);
+    }
 
-    printTemplateService.updateDemoData(vo);
+    /**
+     * 修改
+     */
+    @ApiOperation("修改")
+    @HasPermission({"base-data:print-template:modify"})
+    @PutMapping
+    public InvokeResult<Void> update(@Valid UpdatePrintTemplateVo vo) {
 
-    return InvokeResultBuilder.success();
-  }
+        printTemplateService.update(vo);
 
-  /**
-   * 查询组件设置
-   */
-  @ApiOperation("查询组件设置")
-  @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
-  @HasPermission({"base-data:print-template:query"})
-  @GetMapping("/setting/comp")
-  public InvokeResult<List<GetPrintTemplateCompSettingBo>> getCompSetting(
-      @NotNull(message = "ID不能为空！") Integer id) {
-    List<PrintTemplateComp> datas = new ArrayList<>();
-    datas.addAll(printTemplateCompService.list(Wrappers.lambdaQuery(PrintTemplateComp.class)
-        .eq(PrintTemplateComp::getTemplateId, 0)));
-    datas.addAll(printTemplateCompService.list(Wrappers.lambdaQuery(PrintTemplateComp.class)
-        .eq(PrintTemplateComp::getTemplateId, id)));
+        return InvokeResultBuilder.success();
+    }
 
-    List<GetPrintTemplateCompSettingBo> results = datas.stream()
-        .map(GetPrintTemplateCompSettingBo::new).collect(
-            Collectors.toList());
+    /**
+     * 查询设置
+     */
+    @ApiOperation("查询设置")
+    @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
+    @GetMapping("/setting")
+    public InvokeResult<GetPrintTemplateSettingBo> getSetting(
+            @NotNull(message = "ID不能为空！") Integer id) {
 
-    return InvokeResultBuilder.success(results);
-  }
+        PrintTemplate data = printTemplateService.findById(id);
+        if (data == null) {
+            throw new DefaultClientException("打印模板不存在！");
+        }
+
+        GetPrintTemplateSettingBo result = new GetPrintTemplateSettingBo(data);
+
+        return InvokeResultBuilder.success(result);
+    }
+
+    /**
+     * 保存设置
+     */
+    @ApiOperation("保存设置")
+    @HasPermission({"base-data:print-template:query"})
+    @PutMapping("/setting")
+    public InvokeResult<Void> updateSetting(@Valid UpdatePrintTemplateSettingVo vo) {
+
+        printTemplateService.updateSetting(vo);
+
+        return InvokeResultBuilder.success();
+    }
+
+    @ApiOperation("设置示例数据")
+    @HasPermission({"base-data:print-template:modify"})
+    @PutMapping("/setting/demo")
+    public InvokeResult<Void> updateDemoData(@Valid UpdatePrintTemplateDemoDataVo vo) {
+
+        printTemplateService.updateDemoData(vo);
+
+        return InvokeResultBuilder.success();
+    }
+
+    /**
+     * 查询组件设置
+     */
+    @ApiOperation("查询组件设置")
+    @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
+    @HasPermission({"base-data:print-template:query"})
+    @GetMapping("/setting/comp")
+    public InvokeResult<List<GetPrintTemplateCompSettingBo>> getCompSetting(
+            @NotNull(message = "ID不能为空！") Integer id) {
+        List<PrintTemplateComp> datas = new ArrayList<>();
+        datas.addAll(printTemplateCompService.list(Wrappers.lambdaQuery(PrintTemplateComp.class)
+                .eq(PrintTemplateComp::getTemplateId, 0)));
+        datas.addAll(printTemplateCompService.list(Wrappers.lambdaQuery(PrintTemplateComp.class)
+                .eq(PrintTemplateComp::getTemplateId, id)));
+
+        List<GetPrintTemplateCompSettingBo> results = datas.stream()
+                .map(GetPrintTemplateCompSettingBo::new).collect(
+                        Collectors.toList());
+
+        return InvokeResultBuilder.success(results);
+    }
 }
