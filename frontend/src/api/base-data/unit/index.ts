50005fa3f97162efd32a9d2ec1689c400affb373
bogewang <236598164@qq.com>
2026-07-12 13:02:58 +0800

    单位api类型优化

--- frontend/src/api/base-data/unit/index.ts
+++ frontend/src/api/base-data/unit/index.ts
@@ -1,30 +1,41 @@
-import {defHttp} from '/@/utils/http/axios';
-import {PageResult} from '@/api/model/pageResult';
-import {SortPageVo} from '@/api/model/sortPageVo';
-import {ContentTypeEnum, ResponseEnum} from '@/enums/httpEnum';
+import { defHttp } from '/@/utils/http/axios';
+import { PageResult } from '@/api/model/pageResult';
+import { ContentTypeEnum, ResponseEnum } from '@/enums/httpEnum';
+import { QueryUnitVo } from './model/queryUnitVo';
+import { QueryUnitBo } from './model/queryUnitBo';
+import { CreateUnitVo } from './model/createUnitVo';
+import { UpdateUnitVo } from './model/updateUnitVo';
 
 const url = '/basedata/unit';
-const option = {contentType: ContentTypeEnum.FORM_URLENCODED, region: 'cloud-api'};
+const option = { contentType: ContentTypeEnum.FORM_URLENCODED, region: 'cloud-api' };
 
-export function query(params: SortPageVo & {
-  code?: string;
-  name?: string
-}): Promise<PageResult<any>> {
-  return defHttp.get<PageResult<any>>({url: url + '/query', params}, {region: 'cloud-api'});
+export function query(params: QueryUnitVo): Promise<PageResult<QueryUnitBo>> {
+  return defHttp.get<PageResult<QueryUnitBo>>(
+    { url: url + '/query', params },
+    { region: 'cloud-api' },
+  );
 }
 
-export const generateCode = () =>
-  defHttp.get({url: url + '/generate/code'}, {region: 'cloud-api'});
-export const create = (data: any) => defHttp.post({url, data}, option);
-export const update = (data: any) => defHttp.put({url, data}, option);
-export const remove = (id: string) => defHttp.delete({url, data: {id}}, option);
-export const downloadImportTemplate = () =>
-  defHttp.get(
-    {url: url + '/import/template'},
-    {responseType: ResponseEnum.BLOB, region: 'cloud-api'},
+export const generateCode = (): Promise<string> =>
+  defHttp.get<string>({ url: url + '/generate/code' }, { region: 'cloud-api' });
+
+export const create = (data: CreateUnitVo): Promise<void> =>
+  defHttp.post<void>({ url, data }, option);
+
+export const update = (data: UpdateUnitVo): Promise<void> =>
+  defHttp.put<void>({ url, data }, option);
+
+export const remove = (id: string): Promise<void> =>
+  defHttp.delete<void>({ url, data: { id } }, option);
+
+export const downloadImportTemplate = (): Promise<Blob> =>
+  defHttp.get<Blob>(
+    { url: url + '/import/template' },
+    { responseType: ResponseEnum.BLOB, region: 'cloud-api' },
   );
-export const importExcel = (data: { file: Blob }) =>
-  defHttp.post(
-    {url: url + '/import', data},
-    {contentType: ContentTypeEnum.BLOB, region: 'cloud-api'},
+
+export const importExcel = (data: { file: Blob }): Promise<void> =>
+  defHttp.post<void>(
+    { url: url + '/import', data },
+    { contentType: ContentTypeEnum.BLOB, region: 'cloud-api' },
   );
